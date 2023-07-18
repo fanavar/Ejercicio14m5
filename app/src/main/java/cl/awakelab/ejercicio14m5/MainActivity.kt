818d0f91ae3d44d57c3a263560e5960a869d10b8
Fabián <fanavar@alumnos.ubiobio.cl>
2023-07-17 21:49:55 -0400

    Ejercicio 14 diseño completado + funcionalidades básicas

--- app/src/main/java/cl/awakelab/ejercicio14m5/MainActivity.kt
+++ app/src/main/java/cl/awakelab/ejercicio14m5/MainActivity.kt
@@ -2,10 +2,37 @@ package cl.awakelab.ejercicio14m5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import cl.awakelab.ejercicio14m5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var saldo = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonOk.setOnClickListener{
+            when (binding.radioGroup.checkedRadioButtonId){
+               R.id.radioButtonSaldo ->
+                   binding.textMonto.setText(saldo.toString())
+                R.id.radioButtonIngresar -> ingresarSaldo()
+                R.id.radioButtonRetirar -> retirarSaldo()
+                R.id.radioButtonSalir -> finish()
+
+            }
+        }
     }
+
+    private fun retirarSaldo() {
+        saldo -= binding.textMonto.text.toString().toInt()
+        Toast.makeText(applicationContext,"El retiro ha sido realizado correctamente",Toast.LENGTH_LONG).show()
+    }
+
+    fun ingresarSaldo(){
+    saldo += binding.textMonto.text.toString().toInt()
+    Toast.makeText(applicationContext,"Su saldo ha sido actualizado correctamente",Toast.LENGTH_LONG).show()
+    }
+
 }
